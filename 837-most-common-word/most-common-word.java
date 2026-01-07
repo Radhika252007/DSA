e3b63c0f6fd991d0a6fd2824749b911ecd82ef9b
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-01-07 16:25:55 +0530

    Time: 18 ms (36.17%) | Memory: 45.3 MB (21.64%) - LeetSync

--- 837-most-common-word/most-common-word.java
+++ 837-most-common-word/most-common-word.java
@@ -3,18 +3,18 @@ class Solution {
         paragraph = paragraph.replaceAll("[^a-zA-Z ]", " ").toLowerCase();
         String[] words = paragraph.trim().split("\\s+");
         HashMap<String,Integer> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
         for(int i=0;i<words.length;i++){
             map.put(words[i],map.getOrDefault(words[i],0)+1);
+        }
+        for(int i=0;i<banned.length;i++){
+            set.add(banned[i]);
         }
         int maxFreq = Integer.MIN_VALUE;
         String res = "";
         for(String word : map.keySet()){
-            boolean ban = false;
-            for(int i=0;i<banned.length;i++){
-                if(word.equals(banned[i])){
-                    ban = true;
-                }
-            }
+            boolean ban = set.contains(word) ? true : false ;
+            
             if(!ban){
                 if(map.get(word)>maxFreq){
                     maxFreq = map.get(word);
