b784a0e9b6ac1263833d4cce8bf49524540b9d64
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-02-10 10:07:45 +0530

    Time: 3 ms (92.47%) | Memory: 47.5 MB (6.69%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
         getPermutations(res,nums,0);
         return res;
     }
@@ -10,11 +11,13 @@ class Solution {
             for(int num: nums){
                 curr.add(num);
             }
-            if(res.contains(curr)) return;
             res.add(curr);
             return;
         }
+        HashSet<Integer> set = new HashSet<>();
         for(int i=idx;i<nums.length;i++){
+            if(set.contains(nums[i])) continue;
+            set.add(nums[i]);
             swap(nums,i,idx);
             getPermutations(res,nums,idx+1);
             swap(nums,i,idx);
