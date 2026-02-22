af93a141929ecea2c420e182a5bbad87c4b46d44
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-02-22 17:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (25.87%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -20,10 +20,11 @@ class Solution {
     }
     public void invert(TreeNode root){
         if(root==null) return;
+        invert(root.left);
+        invert(root.right);
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
-        invert(root.left);
-        invert(root.right);
+        
     }
 }
