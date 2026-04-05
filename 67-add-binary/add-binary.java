c390db8f65729302d1e08bd89bd0caa5094b13ff
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-04-05 15:56:43 +0530

    Time: 1 ms (99.97%) | Memory: 43.5 MB (69.42%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -4,7 +4,7 @@ class Solution {
         int j = b.length() - 1;
         int carry = 0;
         StringBuilder res = new StringBuilder();
-        while(i>=0 || j>=0||carry!=0){
+        while(i>=0 || j>=0||carry==1){
             int sum = carry;
             if(i>=0){
                 sum += a.charAt(i) - '0';
