7bbf24fcc56456f2bda5d8966c5c540e390e0fa9
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-04-11 19:01:10 +0530

    Time: 31 ms (40.36%) | Memory: 97.3 MB (11.51%) - LeetSync

---
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int shortestSubarray(int[] nums, int k) {
+        Deque<Integer> q = new LinkedList<>();
+        int ans = Integer.MAX_VALUE;
+        long[] prefix = new long[nums.length+1];
+        for(int i = 0 ;i< nums.length;i++){
+            prefix[i+1] = prefix[i] + nums[i];
+        }
+    
+        for(int i = 0 ;i<=nums.length;i++){
+            while(!q.isEmpty() && prefix[i]- prefix[q.getFirst()] >= k){
+                ans = Math.min(ans,i-q.removeFirst());
+            }
+            while(!q.isEmpty() && prefix[i] <= prefix[q.getLast()]){
+                q.removeLast();
+            }
+            q.addLast(i);
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
