c79b8f201194a3f42aa8a8c211209356758ca7d7
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-04-17 13:04:50 +0530

    Time: 10 ms (75.36%) | Memory: 48.6 MB (48.93%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,22 +1,19 @@
 class Solution {
-    public int[] nextGreaterElements(int[] nums) {
+    public int[] nextGreaterElements(int[] arr) {
+        int[] res = new int[arr.length];
+        Arrays.fill(res,-1);
         Stack<Integer> st = new Stack<>();
-        int[] res = new int[nums.length];
-        for(int i = 0 ; i< nums.length;i++){
-            while(!st.isEmpty() && nums[st.peek()] < nums[i]){
-                int idx = st.pop();
-                res[idx] = nums[i];
+        for(int i = 0;i<arr.length;i++){
+            int num = arr[i];
+            while (!st.isEmpty() && arr[st.peek()] < num){
+               res[st.pop()] = num;
             }
             st.push(i);
         }
-
-        for(int i = 0 ;i<nums.length;i++){
-            while(!st.isEmpty() && nums[st.peek()] < nums[i]){
-                res[st.pop()] = nums[i];
+        for(int i =0;i< arr.length;i++){
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]){
+                res[st.pop()] = arr[i];
             }
-        }
-        while(!st.isEmpty()){
-            res[st.pop()] = -1;
         }
         return res;
     }
