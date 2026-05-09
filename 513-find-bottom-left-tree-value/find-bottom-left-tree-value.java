5ff18c9434d7c9b42f48a1e78a8197c025d4d9a4
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-05-09 15:57:55 +0530

    Time: 3 ms (68.39%) | Memory: 46 MB (97.00%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -17,18 +17,17 @@ class Solution {
     public int findBottomLeftValue(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        List<List<Integer>> list = new ArrayList<>();
+        int res = 0;
         while(!q.isEmpty()){
-            List<Integer> curr = new ArrayList<>();
+            res = q.peek().val;
             int levelSize = q.size();
             for(int i = 0;i<levelSize;i++){
                 TreeNode currNode = q.poll();
-                curr.add(currNode.val);
                 if(currNode.left!=null) q.offer(currNode.left);
                 if(currNode.right!=null) q.offer(currNode.right);
             }
-            list.add(new ArrayList(curr));
+           
         }
-        return list.get(list.size()-1).get(0);
+        return res;
     }
 }
