1e282b805e28145274f8d32070abfa225463949a
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-06-17 17:33:21 +0530

    Time: 3 ms (76.75%) | Memory: 47.4 MB (21.76%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -17,21 +17,15 @@ class Solution {
     Set<Integer> set;
     public boolean findTarget(TreeNode root, int k) {
          set = new HashSet<>();
-         preorder(root);
-         return preorder2(root,k);
+         return findSum(root,k);
 
          
     }
-    public void preorder(TreeNode root){
-        if(root == null) return;
-        set.add(root.val);
-        preorder(root.left);
-        preorder(root.right);
-    }
-    public boolean preorder2(TreeNode root, int k){
+    public boolean findSum(TreeNode root, int k){
         if(root == null) return false;;
         if(k - root.val != root.val && set.contains(k - root.val)) return true;
-        return preorder2(root.left,k) || preorder2(root.right,k);
+        set.add(root.val);
+        return findSum(root.left,k) || findSum(root.right,k);
     }
     
 }
