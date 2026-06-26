e7a0173d9143cf7b5887cf34da150ba25c2d4875
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-06-26 19:30:44 +0530

    Time: 7 ms (100.00%) | Memory: 47.6 MB (79.45%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,46 +1,22 @@
 class NumArray {
-    class Node{
-        int val;
-        int startInterval;
-        int endInterval;
-        Node left;
-        Node right;
-        Node(int startInterval, int endInterval){
-            this.startInterval = startInterval;
-            this.endInterval = endInterval;
-        }
-    }
-    Node root;
+    int[] nums;
+    int[] prefix;
     public NumArray(int[] nums) {
-        this.root = constructTree(nums, 0, nums.length-1);
+        this.nums = nums;
+        prefix = new int[nums.length + 1];
+        calculatePrefix();
     }
-    private Node constructTree(int[] arr, int start, int end){
-        if(start==end){
-            Node newNode = new Node(start,end);
-            newNode.val = arr[start];
-            return newNode;
+    public void calculatePrefix(){
+        prefix[0] = 0;
+        int n = prefix.length;
+        for(int i =1;i<n;i++){
+            prefix[i] = prefix[i-1] + nums[i-1];
         }
-        Node node = new Node(start,end);
-        int mid = start + (end - start) / 2;
-        node.left = constructTree(arr,start,mid);
-        node.right = constructTree(arr,mid+1,end);
-        node.val = node.left.val + node.right.val;
-        return node;
     }
+    
     public int sumRange(int left, int right) {
-        if(root==null) return 0;
-        return getSum(root,left,right);
+        return prefix[right + 1] - prefix[left];
     }
-    private int getSum(Node node, int left, int right){
-        if(node.startInterval >=left && node.endInterval <=right){
-            return node.val;
-        }
-        if(node.endInterval < left || node.startInterval > right){
-            return 0;
-        }
-        return getSum(node.left,left,right) +getSum(node.right,left,right);
-    }
-
 }
 
 /**
