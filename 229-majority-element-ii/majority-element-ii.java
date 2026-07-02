387a18c93785d65c92c04ec9ff760762cdece343
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-07-02 12:37:05 +0530

    Time: 2 ms (99.94%) | Memory: 52.5 MB (52.54%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,15 +1,35 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        LinkedHashSet<Integer> set = new LinkedHashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.get(nums[i])>(nums.length)/3){
-                set.add(nums[i]);
+        int c1 = 0;
+        int c2 = 0;
+        int el1 =Integer.MIN_VALUE;
+        int el2 =Integer.MIN_VALUE;
+        for(int i = 0;i<nums.length;i++){
+            if(c1 == 0 && nums[i] != el2){
+                c1 = 1;
+                el1 = nums[i];
+            }
+            else if(c2 == 0 && nums[i] != el1){
+                c2 = 1;
+                el2 = nums[i];
+            }
+            else if(nums[i] == el1) c1++;
+            else if(nums[i] == el2) c2++;
+            else{
+                c1--;
+                c2--;
             }
         }
-        return new ArrayList<Integer>(set);
+        c1 = 0;
+        c2 = 0;
+        for(int val : nums){
+            if(val == el1) c1++;
+            if(val == el2) c2++;
+        }
+        List<Integer> res = new ArrayList<>();
+        int n = nums.length;
+        if(c1 > n/3) res.add(el1);
+        if(c2 > n/3) res.add(el2);
+        return res;
     }
 }
