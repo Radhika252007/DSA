363ef0b7751191c86560f7f3cccc78b849e2047e
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-07-31 19:59:27 +0530

    Time: 9 ms (94.22%) | Memory: 48.3 MB (17.34%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,16 +1,17 @@
 class Solution {
     public int minimumPushes(String word) {
-       HashMap<Character, Integer> map = new HashMap<>();
-       for(int i = 0;i<word.length();i++){
-        char c = word.charAt(i);
-        map.put(c, map.getOrDefault(c,0)+1);
-       }
-       List<Integer> list = new ArrayList<>(map.values());
-       list.sort(Collections.reverseOrder());
-       int count = 0;
-       for(int i =0;i<list.size();i++){
-          count += ((i / 8) + 1) * list.get(i);
-       }
-       return count;
+        int[] freq = new int[26];
+        for(int i = 0;i<word.length();i++){
+            freq[word.charAt(i) - 'a']++;
+        }
+        Arrays.sort(freq);
+        int count = 0;
+        int j = 0;
+        for(int i =  25;i>=0;i--){
+            if(freq[i] == 0) break;
+            count += ((j / 8) + 1) * freq[i];
+            j++;
+        }
+        return count;
     }
 }
