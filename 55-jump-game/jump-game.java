0361bacb52bfd57fa8e916cc195bdaa7694a39b9
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-08-01 20:47:20 +0530

    Time: 2 ms (88.65%) | Memory: 47.8 MB (81.74%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -3,7 +3,9 @@ class Solution {
         int maxIdx = 0;
         for(int i = 0;i<nums.length;i++){
             if(i > maxIdx) return false;
-            maxIdx = Math.max(maxIdx, i + nums[i]);
+            if(i + nums[i] > maxIdx){
+                maxIdx = i + nums[i];
+            }
         }
         return true;
     }
