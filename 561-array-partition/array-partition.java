a1f34c34c6756b1547e469ec689d3689b7888e2e
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-08-01 21:11:09 +0530

    Time: 17 ms (81.32%) | Memory: 49.8 MB (36.42%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -2,14 +2,10 @@ class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        int i = n-1;
-        int j =n-2;
-        int sum = 0;
-        while(j >= 0 && i>=1){
-            sum += Math.min(nums[i],nums[j]);
-            j-=2;
-            i-=2;
-        }
+       int sum = 0;
+       for(int i = 0;i<n;i+=2){
+        sum += nums[i];
+       }
         return sum;
     }
 }
