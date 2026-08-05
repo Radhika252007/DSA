a0fe674b93d7174246e5ec31639171c32e987202
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-08-05 11:48:19 +0530

    Time: 10 ms (21.99%) | Memory: 49.2 MB (27.49%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,30 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(intervals , (a,b) -> Integer.compare(a[0],b[0]));
-        for(int i = 0;i<intervals.length;i++){
-            if(list.isEmpty() || list.get(list.size() - 1).get(1) < intervals[i][0]){
-                List<Integer> sub = new ArrayList<>();
-                sub.add(intervals[i][0]);
-                sub.add(intervals[i][1]);
-                list.add(sub);
+        Arrays.sort(intervals, (a,b)->a[0] - b[0]);
+        int[] curr = intervals[0];
+        ArrayList<int[]> list = new ArrayList<>();
+        for(int i = 1;i<intervals.length;i++){
+            if(intervals[i][0] > curr[1]){
+                list.add(curr);
+                curr = intervals[i];
             }
             else{
-                int last = list.size() - 1;
-                int maxVal = Math.max(list.get(last).get(1) , intervals[i][1]);
-                list.get(last).set(1,maxVal);
+                curr[0] = Math.min(curr[0], intervals[i][0]);
+                curr[1] = Math.max(curr[1], intervals[i][1]);
             }
         }
-        int rows = list.size();
-int cols = 2;
-
-int[][] matrix = new int[rows][cols];
-
-for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < cols; j++) {
-        matrix[i][j] = list.get(i).get(j);
-    }
-}
-return matrix;
+        list.add(curr);
+        return list.toArray(new int[list.size()][]);
     }
 }
