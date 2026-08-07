ea210f7ba2f768dc6faed3339b639297abfddd84
Radhika252007 <mukeshmadaan2025@gmail.com>
2026-08-07 12:06:05 +0530

    Time: 2481 ms (21.08%) | Memory: 42.9 MB (69.78%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,26 +1,20 @@
 class Solution {
-    int[] cuts;
     public int minCut(String s) {
         int n = s.length();
-        cuts = new int[n];
-        Arrays.fill(cuts,-1);
-        return minCutHelper(s,0);
-    }
-    public int minCutHelper(String s, int start){
-        if(start == s.length() || isPal(s,start,s.length()-1)){
-            return 0;
-        }
-        if(cuts[start] != -1) return cuts[start];
-        int min = Integer.MAX_VALUE;
-        for(int end = start;end < s.length();end++){
-            if(isPal(s,start,end)){
-                int cuts = 1 + minCutHelper(s,end+1);
-                min = Math.min(cuts,min);
+        int[] cuts = new int[n+1];
+        cuts[n] = -1;
+        for(int start = n-1;start>=0;start--){
+            int minCuts = Integer.MAX_VALUE;
+            for(int end = start;end<n;end++){
+                if(isPal(s,start,end)){
+                    minCuts = Math.min(1 + cuts[end + 1], minCuts);
+                }
             }
-            
+            cuts[start] = minCuts;
         }
-        return cuts[start] = min;
+        return cuts[0];
     }
+    
     public boolean isPal(String s, int start, int end){
         while(start < end){
             if(s.charAt(start) != s.charAt(end)) return false;
